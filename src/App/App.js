074195987aa281d315dbafe38ffb91c2bc5d9845
Dana Zack <danaezack@gmail.com>
2024-02-24 14:28:11 -0700

    Feat: set up additional starting components

--- src/App/App.js
+++ src/App/App.js
@@ -5,14 +5,14 @@ import { getMeds } from '../apiCalls';
 
 function App() {
   const [meds, setMeds] = useState([]);
-  
+
   useEffect(() => {
     getMeds()
-      .then(data => setMeds(data.drugGroup.conceptGroup.find(result => result.tty === "SBD").conceptProperties))
+      .then(data => setMeds(data.drugGroup.conceptGroup.find(result => result.tty === "SBD").conceptProperties.map(result => result.name)))
   }, [])
-  
+
   return (
-      <Home meds={meds} />
+    <Home meds={meds} />
   );
 }
 
