89e663809798e3db221d66fab4b28647ee00612b
Dana Zack <danaezack@gmail.com>
2024-03-02 15:05:31 -0700

    Refactor: add UI responsiveness

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -9,12 +9,19 @@ function Form({ quantity, setQuantity, frequency, setFrequency }) {
         <option value='0.5'>0.5</option>
         <option value='1'>1</option>
         <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
       </select>
       <select value={frequency} className='frequency dropdown' onChange={(event) => setFrequency(event.target.value)}>
         <option value=''>Specify Frequency</option>
         <option value='1x daily'>1x daily</option>
-        <option value= '2x daily'>2x daily</option>
+        <option value='2x daily'>2x daily</option>
         <option value='3x daily'>3x daily</option>
+        <option value='4x daily'>4x daily</option>
+        <option value='1x weekly'>1x weekly</option>
+        <option value='2x weekly'>2x weekly</option>
+        <option value='as needed'>as needed</option>
       </select>
     </form>
   );
