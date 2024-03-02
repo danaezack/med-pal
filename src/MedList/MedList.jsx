029be6dea38c43b7c546692807112d8f68bfed0e
Dana Zack <danaezack@gmail.com>
2024-03-01 17:15:31 -0700

    Feat: display message to user if med list is empty

--- src/MedList/MedList.jsx
+++ src/MedList/MedList.jsx
@@ -18,7 +18,7 @@ function MedList({ medList }) {
   return (
     <main className='med-list-page'>
       <h2 className='med-list-title'>My Medication List:</h2>
-      {savedCards}
+      { medList.length ? savedCards : <p>There are currently no medications in your medication list.</p> }
       <Link to='/'>
         <button className='return-btn'>Click to add more</button>
       </Link>
